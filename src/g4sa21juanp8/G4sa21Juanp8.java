a563fa70f82f75bbd3ebd24a21aa30306bc537bc
Juan <>
2022-04-06 12:13:36 -0500

    PRACTICA 8

---
+++ src/g4sa21juanp8/G4sa21Juanp8.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4sa21juanp8;
+
+/**
+ *
+ * @author user
+ */
+public class G4sa21Juanp8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new principal().setVisible(true);
+    }
+    
+}
